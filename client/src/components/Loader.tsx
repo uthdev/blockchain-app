e0546fdafb38b45081942f828aa0033dc5b6efbc
uthdev <adelekegbolahan92@yahoo.com>
2022-01-15 01:47:24 +0100

    implement solidity smart contract

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const Loader = (props: Props) => {
   return (
-    <div>
-      <h1>Loader</h1>
-    </div>
+  <div className="flex justify-center items-center py-3">
+    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700" />
+  </div>
   )
 }
 
---
+++ client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const CONTRACT_ABI = abi.abi;
+export const CONTRACT_ADDRESS = '0xb5A1f6c7c5E4c56A5f7e3384dfCa9971aa7B1504'
